d7e6b7d85f8245d6f79ac035f0559023c9daa772
Riya Parikh <moodycodes@gmail.com>
2021-10-10 13:38:29 +0530

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Leaderboard from './components/leaderboard';
+import './App.css'
+import Leaderboard from './components/leaderboard'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Landing from './components/Landing'
 function App() {
   return (
-    <div className="App">
-      <Leaderboard/>
+    <div className='App'>
+      <Landing />
+      {/* <Leaderboard /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
